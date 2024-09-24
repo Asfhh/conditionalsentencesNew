ed8448d95b450ffba118fbb59461dec1a564c969
Asfhh <keblaitis.j@gmail.com>
2024-09-24 18:37:45 +0300

    complete conditional sentence

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
+print("Įveskite žodį")
+word = input()
 
+if len(word) < 5:
+    print(f'žodis {word} yra trumpas')
 
+elif len(word) < 10:
+    print(f'žodis {word} yra vidutinio ilgio')
 
+else:
+    print(f'žodis {word} yra ilgas')
 
-if 7:
-    print("hello")
+if 5 > 4 and 3 < 8 and 7 < 14:
+    print("visos sąlygos teisingos")
 
-    if 7:
-        print("ok")
+if 5 > 4 or 3 < 8 or 7 < 14:
+    print("bent viena sąlyga teisinga")
+
+# if 7:
+#     print("hi")
+# elif 8:
+#     print("yes")
+# elif 9:
+#     print("no")
+# else("yey")
+
